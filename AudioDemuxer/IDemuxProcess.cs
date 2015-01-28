35454312aef83c07ff7f2699c1c6a694b75d4ce7
tom19960222 <tom19960222@gmail.com>
2015-01-29 02:28:03 +0800

    Refactor
    Refactor with abstract class.

--- AudioDemuxer/IDemuxProcess.cs
+++ AudioDemuxer/IDemuxProcess.cs
@@ -1,9 +1,43 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
 namespace AudioDemuxer
 {
     interface IDemuxProcess
     {
         void Dispose();
         void Start();
+    }
+
+    public abstract class DemuxProcessUsingOneCommand : IDemuxProcess
+    {
+        protected Process P;
+        public void Start()
+        {
+            P.Start();
+            P.WaitForExit();
+        }
+        public void Dispose()
+        {
+            P.Dispose();
+        }
+    }
+
+    public abstract class DemuxProcessUsingMutilCommand : IDemuxProcess
+    {
+        protected List<Process> ProcessList;
+        public void Start()
+        {
+            foreach (Process Pro in ProcessList)
+            { Pro.Start(); Pro.WaitForExit(); }
+        }
+        public void Dispose()
+        {
+            foreach (Process P in ProcessList)
+            {
+                P.Dispose();
+            }
+            ProcessList.Clear();
+        }
     }
 }
--- AudioDemuxer/Tools.cs
+++ AudioDemuxer/Tools.cs
@@ -15,14 +15,12 @@ namespace AudioDemuxer
     {
         public class MP4Box
         {
-            private static string ToolsBaseDirectory { get { return Environment.CurrentDirectory + "\\tools\\mp4box\\"; } }
+            private static string ToolsBaseDirectory = Environment.CurrentDirectory + "\\tools\\mp4box\\";
             private static string BinaryFileName = "mp4box.exe";
             public static string ProgramPath { get { return ToolsBaseDirectory + BinaryFileName; } }
             public static string ParameterBuilder(string TrackID, string SourceFileName)
             {
-                string Parameter = String.Empty;
-                Parameter += String.Format(" -raw {0} \"{1}\"", TrackID, SourceFileName);
-                return Parameter;
+                return String.Format(" -raw {0} \"{1}\"", TrackID, SourceFileName);;
             }
             public static string CommandBuilder(ListDictionary TrackID_SourceFileName_Pair)
             {
@@ -31,9 +29,8 @@ namespace AudioDemuxer
                     Command += String.Format("\"{0}\" {1}{2}",ProgramPath, ParameterBuilder(Pair.Key.ToString(), Pair.Value.ToString()), Environment.NewLine);
                 return Command;
             }
-            public class MP4DemuxProcess : IDemuxProcess
+            public class MP4DemuxProcess : DemuxProcessUsingMutilCommand, IDemuxProcess
             {
-                private List<Process> ProcessList;
                 public MP4DemuxProcess(ListDictionary TrackID_SourceFileName_Pair)
                 {
                     ProcessList = new List<Process>();
@@ -45,24 +42,11 @@ namespace AudioDemuxer
                         ProcessList.Add(P);
                     }
                 }
-                public void Start()
-                {
-                    foreach (Process Pro in ProcessList)
-                    { Pro.Start(); Pro.WaitForExit(); }
-                }
-                public void Dispose()
-                {
-                    foreach (Process P in ProcessList)
-                    {
-                        P.Dispose();
-                    }
-                    ProcessList.Clear();
-                }
             }
         }
         public class MKVExtract
         {
-            private static string ToolsBaseDirectory { get { return Environment.CurrentDirectory + "\\tools\\mkvmerge\\"; } }
+            private static string ToolsBaseDirectory = Environment.CurrentDirectory + "\\tools\\mkvmerge\\"; 
             private static string BinaryFileName = "mkvextract.exe";
             private static string ProgramParameter = "tracks";
             public static string ProgramPath { get { return ToolsBaseDirectory + BinaryFileName; } }
@@ -78,29 +62,19 @@ namespace AudioDemuxer
             {
                 return ProgramPath + ParameterBuilder(TrackID_OutputFileName_Pair, SourceFileName);
             }
-            public class MKVDemuxProcess : IDemuxProcess
+            public class MKVDemuxProcess : DemuxProcessUsingOneCommand, IDemuxProcess
             {
-                private Process P;
                 public MKVDemuxProcess(ListDictionary TrackID_OutputFileName_Pair, string SourceFileName)
                 {
                     P = new Process();
                     P.StartInfo.FileName = ProgramPath;
                     P.StartInfo.Arguments = ParameterBuilder(TrackID_OutputFileName_Pair, SourceFileName);
                 }
-                public void Start()
-                {
-                    P.Start();
-                    P.WaitForExit();
-                }
-                public void Dispose()
-                {
-                    P.Dispose();
-                }
             }
         }
         public class eac3to
         {
-            private static string ToolsBaseDirectory { get { return Environment.CurrentDirectory + "\\tools\\eac3to\\"; } }
+            private static string ToolsBaseDirectory = Environment.CurrentDirectory + "\\tools\\eac3to\\";
             private static string BinaryFileName = "eac3to.exe";
             public static string ProgramPath { get { return ToolsBaseDirectory + BinaryFileName; } }
             public static string ParameterBuilder(ListDictionary TrackID_OutputFileName_Pair, string SourceFileName)
@@ -115,56 +89,22 @@ namespace AudioDemuxer
             {
                 return ProgramPath + ParameterBuilder(TrackID_OutputFileName_Pair, SourceFileName);
             }
-            public static string AnalyseByEac3to(string FileName)
+            public class M2TSDemuxProcess : DemuxProcessUsingOneCommand, IDemuxProcess
             {
-                string result = String.Empty;
-
-                Process eac3to_Analyse = new Process();
-                eac3to_Analyse.StartInfo.FileName = eac3to.ToolsBaseDirectory + eac3to.BinaryFileName;
-                eac3to_Analyse.StartInfo.Arguments = " " + FileName;
-                eac3to_Analyse.StartInfo.RedirectStandardOutput = true;
-                eac3to_Analyse.StartInfo.UseShellExecute = false;
-                eac3to_Analyse.Start();
-                result = eac3to_Analyse.StandardOutput.ReadToEnd();
-                eac3to_Analyse.WaitForExit();
-                return result;
-            }
-            public class M2TSDemuxProcess : IDemuxProcess
-            {
-                Process P;
                 public M2TSDemuxProcess(ListDictionary TrackID_OutputFileName_Pair, string SourceFileName)
                 {
                     P = new Process();
                     P.StartInfo.FileName = ProgramPath;
                     P.StartInfo.Arguments = ParameterBuilder(TrackID_OutputFileName_Pair, SourceFileName);
                 }
-                public void Start()
-                {
-                    P.Start();
-                    P.WaitForExit();
-                }
-                public void Dispose()
-                {
-                    P.Dispose();
-                }
             }
-            public class TSDemuxProcess : IDemuxProcess
+            public class TSDemuxProcess : DemuxProcessUsingOneCommand, IDemuxProcess
             {
-                Process P;
                 public TSDemuxProcess(ListDictionary TrackID_OutputFileName_Pair, string SourceFileName)
                 {
                     P = new Process();
                     P.StartInfo.FileName = ProgramPath;
                     P.StartInfo.Arguments = ParameterBuilder(TrackID_OutputFileName_Pair, SourceFileName);
-                }
-                public void Start()
-                {
-                    P.Start();
-                    P.WaitForExit();
-                }
-                public void Dispose()
-                {
-                    P.Dispose();
                 }
             }
 
